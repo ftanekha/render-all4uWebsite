e4a3442cfaede8fa17898800dac2c40389fb4a6b
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2022-07-05 16:51:52 +0100

    correct company contact number remove unnecessary terms and conditions page capture form data add validation to privacy policy checkbox element

--- dist/control/registrationFormSubmit.js
+++ dist/control/registrationFormSubmit.js
@@ -12,29 +12,60 @@ document.addEventListener('DOMContentLoaded', ()=>{
         'click', ev => {
             ev.preventDefault()
 
-            registrationPage.style.display = 'none'
-            spinner.style.display = 'block'
+            const privacyPolicy = document.querySelector('#candidate_privacy_policy_consent')
+            //capture form data
+            function captureFormData(){
+                const
+                personalInfo = {
+                    title: document.querySelector('#candidate_title').value,
+                    forename: document.querySelector('#candidate_forename').value,
+                    surname: document.querySelector('#candidate_surname').value,
+                    dob: document.querySelector('#candidate_dob').value,
+                    gender: document.querySelector('#candidate_gender').value,
+                    nationality: document.querySelector('#candidate_nationality').value,
+                },
+                contactDetails = {
+                    phone: document.querySelector('#candidate_contact_number').value,
+                    email: document.querySelector('#candidate_email_address').value,
+                    address: document.querySelector('#candidate_address').value,
+                    town: document.querySelector('#candidate_address_town').value,
+                    county: document.querySelector('#candidate_address_county').value,
+                    pcode: document.querySelector('#candidate_address_p_code').value
+                }
 
-            const 
-            showSpinner = setInterval(
-                ()=> spinner.style.display = 'none', 5e3
-            ),
-            showConfirmationMessage = setTimeout(
-                ()=> {
-                    //reset form input fields
-                    registrationForm.reset()
-                    clearInterval(showSpinner)
-                    confirmationMessage.style.display = 'block'
-                }, 5e3
-            ),
-            returnToHomePage = setTimeout(
-                ()=>{
-                    confirmationMessage.style.display = 'none'
-                    clearTimeout(showConfirmationMessage)
-                    clearTimeout(returnToHomePage)
-                    registrationPage.style.display = 'block'
-                }, 1.5e4
-            )
+                for(let i of Object.values(personalInfo)) console.log(i)
+                for(let i of Object.values(contactDetails)) console.log(i)
+            } 
+            captureFormData()
+            //comfirm registration complete
+            function confirmRegistration () {
+                registrationPage.style.display = 'none'
+                spinner.style.display = 'block'
+
+                const 
+                showSpinner = setInterval(
+                    ()=> spinner.style.display = 'none', 5e3
+                ),
+                showConfirmationMessage = setTimeout(
+                    ()=> {
+                        //reset form input fields
+                        registrationForm.reset()
+                        clearInterval(showSpinner)
+                        confirmationMessage.style.display = 'block'
+                    }, 5e3
+                ),
+                returnToHomePage = setTimeout(
+                    ()=>{
+                        confirmationMessage.style.display = 'none'
+                        clearTimeout(showConfirmationMessage)
+                        clearTimeout(returnToHomePage)
+                        registrationPage.style.display = 'block'
+                    }, 1.5e4
+                )
+            }
+            //make sure the privacy policy has been checked
+            privacyPolicy.checked ? confirmRegistration() : alert('Please confirm that you agree to our Privacy Policy?')
+            
         }
     )
 })
